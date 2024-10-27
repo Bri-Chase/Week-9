64c35544874f93c1502a879c1e52f03016216fca
Bri-Chase <brianna.chase@sinclair.edu>
2024-10-27 18:30:09 -0400

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+// Brianna Chase
+// CIS 1202 501
+// Prof. Melissa Goodall
+// October 27th, 2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+	return 0;
+}
